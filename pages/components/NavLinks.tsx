6481844e9969f8c12fd0beb1b1d941bed3fa2022
edmondgaks <edmondgaks@gmail.com>
2023-05-11 23:34:58 +0200

    adding some shit

--- pages/components/NavLinks.tsx
+++ pages/components/NavLinks.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 
 
-
 const NavLinks = () => {
     return (
-        <div className=""></div>
+        <div className="flex flex-row bg-black h-[50px] gap-8 w-[100%] justify-center py-3">
+            <div>HOME</div>
+            <div>ABOUT US</div>
+            <div>MARKET DATA</div>
+            <div>FAQS</div>
+            <div>CONTACT US</div>
+            <div>REGISTER NOW</div>
+            <div>LOGIN NOW</div>
+        </div>
     )
 }
 
--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,34 +1,43 @@
 import React from "react"
 import Image from "next/image";
 import AuthButtons from "./AuthButtons";
+import NavLinks from "./NavLinks";
 const Navbar = () => {
     return (
-        <div className="flex flex-row  w-[100%] gap-10 p-8 bg-[#1d1d1d] justify-between place-items-cente">
-            <Image src="/logo.png" width={200} height={80} alt="Logoo" />
-            <div className="flex gap-10 text-semibold">
-                <div className="flex flex-col">
-                    <div className="font-semibold">9,450 USD</div>
-                    <div className="font-light text-xs">Last trade price</div>
-                </div>
-                <div>
-                    <div className="font-semibold">+5.26%</div>
-                    <div className="font-light text-xs">24 hour price</div>
-                </div>
-                <div>
-                    <div className="font-semibold">12.820 BTC</div>
-                    <div className="font-light text-xs">24 hour volume</div>
-                </div>
-                <div>
-                    <div className="font-semibold">2,231,775 </div>
-                    <div className="font-light text-xs">Active traders</div>
-                </div>
-                <div>
-                    <div className="font-semibold text-[#fd961a]">26854.84</div>
-                    <div className="font-light text-xs">Live bitcoin price</div>
+        <>
+            <div className="flex flex-col w-[100%] p-6 bg-[#1d1d1d] ">
+                <div className="flex flex-row gap-10 justify-between place-items-center">
+                    <Image src="/logo.png" width={200} height={80} alt="Logoo" />
+                    <div className="flex gap-10 text-semibold">
+                        <div className="flex flex-col">
+                            <div className="font-semibold">9,450 USD</div>
+                            <div className="font-light text-xs">Last trade price</div>
+                        </div>
+                        <div>
+                            <div className="font-semibold">+5.26%</div>
+                            <div className="font-light text-xs">24 hour price</div>
+                        </div>
+                        <div>
+                            <div className="font-semibold">12.820 BTC</div>
+                            <div className="font-light text-xs">24 hour volume</div>
+                        </div>
+                        <div>
+                            <div className="font-semibold">2,231,775 </div>
+                            <div className="font-light text-xs">Active traders</div>
+                        </div>
+                        <div>
+                            <div className="font-semibold text-[#fd961a]">26854.84</div>
+                            <div className="font-light text-xs">Live bitcoin price</div>
+                        </div>
+                    </div>
+                    <AuthButtons />
                 </div>
             </div>
-            <AuthButtons />
-        </div>
+            <NavLinks />
+            <div className="flex flex-col bg-[#1d1d1d] h-[40px]">
+                <div>Selectioner un language</div>
+            </div>
+        </>
     )
 }
 export default Navbar
