59437e5f49f633e722d505ec9816c22596798529
edmondgaks <edmondgaks@gmail.com>
2023-05-11 22:20:37 +0200

    styling prices

---
+++ pages/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import React from "react"
+import Image from "next/image";
+const Navbar = () => {
+    return (
+        <div className="flex flex-row  w-full gap-10 p-8 bg-[#1d1d1d]">
+            <Image src="/logo.png" width={200} height={80} alt="Logoo" />
+            <div className="flex gap-10 text-semibold">
+                <div className="flex flex-col">
+                    <div className="font-semibold">9,450 USD</div>
+                    <div className="font-light text-xs">Last trade price</div>
+                </div>
+                <div>
+                    <div className="font-semibold">+5.26%</div>
+                    <div className="font-light text-xs">24 hour price</div>
+                </div>
+                <div>
+                    <div className="font-semibold">12.820 BTC</div>
+                    <div className="font-light text-xs">24 hour volume</div>
+                </div>
+                <div>
+                    <div className="font-semibold">2,231,775 </div>
+                    <div className="font-light text-xs">Active traders</div>
+                </div>
+                <div>
+                    <div className="font-semibold">26854.84</div>
+                    <div className="font-light text-xs">Live bitcoin price</div>
+                </div>
+            </div>
+            <div></div>
+        </div>
+    )
+}
+export default Navbar
